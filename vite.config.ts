9146c8e4b188c6b9c5b019417e8c3c612facb448
Jorge Luna <jorge@pricerqx.com>
2025-10-24 09:03:03 -0400

    path adjustment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,10 @@ import { fileURLToPath, URL } from 'node:url'
 // https://vite.dev/config/
 export default defineConfig(() => {
   const rawBase = process.env.VITE_BASE_PATH
-  const base = rawBase ? `${rawBase.replace(/\/?$/, '/')}` : '/'
+  let base = rawBase ?? './'
+  if (!base.endsWith('/')) {
+    base = `${base}/`
+  }
 
   return {
     base,
